d027a82f3d80ee51cc823f06a942cbaf277983d1
dima <dima-danilov96@mail.ru>
2023-05-22 16:56:10 +0700

    сохранение изменений 2

--- Program.cs
+++ Program.cs
@@ -7,4 +7,18 @@ int result = number * number;
 
 Console.WriteLine ("Число " + number + " в квадрате будет равно: " + result);
 
-//
+// является ли первое число квадратом второго.
+Console.Write("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstnumber == secondnumber * secondnumber)
+{
+    Console.WriteLine ("Число " + firstnumber + " является квадратом числа: " + secondnumber);
+}
+else
+{
+    Console.WriteLine ("Число " + firstnumber + " НЕ является квадратом числа: " + secondnumber);
+}
